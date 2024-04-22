b8e0b531168892614dbe0ffa2ee64f12676573f9
grolston <grolston@amazon.com>
2024-04-22 14:52:42 -0600

    url update

--- pathfinder/pathfinder.sh
+++ pathfinder/pathfinder.sh
@@ -17,6 +17,5 @@ for i in \
    ; do npm install "$i" --silent; done
 
 echo "installation complete, starting pathfinder..."
-# EXAMPLE URL...will change....
-download_url="https://raw.githubusercontent.com/cloud-foundations-on-aws/cloud-foundations-templates/feat-pathfinder/pathfinder/dist/pathfinder.js"
+download_url="https://raw.githubusercontent.com/cloud-foundations-on-aws/cloud-foundations-templates/main/pathfinder/dist/pathfinder.js"
 curl -sS $download_url | node
